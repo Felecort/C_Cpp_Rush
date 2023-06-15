691ad406a703c7e836d6ee323fd2587e3e037699
Felecort <dotpointen@gmail.com>
2023-06-15 10:35:47 +0300

    refactor

--- Cource_selfedu/18_random.c
+++ Cource_selfedu/18_random.c
@@ -13,8 +13,8 @@ int main(){
     printf("time = %d\nr1 = %d\nr2 = %d\n%d\n", t, r1, r2, rand());
 
     int range = 10;
-    r1 = (rand() % 10) - 5;
-    r2 = (rand() % 10) - 5;
+    r1 = (rand() % range) - 5;
+    r2 = (rand() % range) - 5;
     double r_float = (double)rand() / (double)RAND_MAX;
     printf("r1 = %d\nr2 = %d\nr_float = %.2f\n", r1, r2, r_float);
 
