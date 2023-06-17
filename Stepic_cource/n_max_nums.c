d06a37fb6ebc9d6904691fd77cc583b253a4c53e
FriLDD <dotpointen@gmail.com>
2023-06-18 00:22:15 +0300

    refactor

--- Stepic_cource/n_max_nums.c
+++ Stepic_cource/n_max_nums.c
@@ -13,16 +13,17 @@ typedef struct node{
     int value;
 } node;
 
+
+
 int* topKFrequent(int* nums, int num_elements, int k, int* returnSize){
     size_t node_size = sizeof(node);
     int filled_nodes = 0;
     int element;
-    node *struct_array = malloc(num_elements * node_size);
+    node *struct_array = (node *)malloc(num_elements * node_size);
 
     struct_array[0].value = nums[0];
     struct_array[0].counter = 1;
     filled_nodes++;
-    printf("Start filling \n");
     for (int i = 1; i < num_elements; i++){
         element = nums[i];
         for (int node_idx = 0; node_idx < filled_nodes; node_idx++){
@@ -38,10 +39,20 @@ int* topKFrequent(int* nums, int num_elements, int k, int* returnSize){
             }
         }
     }
-    printf("Stop filling \n");
 
     for (int i = 0; i < filled_nodes; i++){
         printf("Value: %d Counter: %d \n", struct_array[i].value, struct_array[i].counter);
+    }
+
+    int max_arr[k];
+    int elem_counter = 0;
+    for (int i = 0; i < k; i++) max_arr[i] = 0;
+
+    for (int idx = 0; idx < filled_nodes; idx++){
+        elem_counter = struct_array[idx].counter;
+        for (int j = 0; j < k; j++){
+            
+        }
     }
 }
 
@@ -49,7 +60,6 @@ int main(void)
 {
     int arr[] = {1, 1, 2, 3, 3, 4, 1, 12, 3, 4, 5, 4, 1, 2, 5};
     int num_elements = sizeof(arr) / sizeof(arr[0]);
-    printf("size arr: %d \n", num_elements);
     int k = 2;
     int *returnSize;
     int *out = topKFrequent(arr, num_elements, k, returnSize);
