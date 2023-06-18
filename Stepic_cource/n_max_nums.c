556141a8b87b1cc34b1178b6b2940fb7ecf216f3
FriLDD <dotpointen@gmail.com>
2023-06-18 11:05:08 +0300

    buf "1 element" fixed

--- Stepic_cource/n_max_nums.c
+++ Stepic_cource/n_max_nums.c
@@ -5,8 +5,9 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include <time.h>
+
+
+// #define DEBUG
 
 typedef struct node{
     int counter;
@@ -15,10 +16,18 @@ typedef struct node{
 
 
 
-int* topKFrequent(int* nums, int num_elements, int k, int* returnSize){
+int* topKFrequent(int* nums, int num_elements, int k, int* return_size){
     size_t node_size = sizeof(node);
     int filled_nodes = 0;
     int element;
+
+    if (num_elements == 1){
+        int *max_values = malloc(sizeof(int));
+        max_values[0] = nums[0];
+        *return_size = 1;
+        return max_values;
+    }
+
     node *struct_array = (node *)malloc(num_elements * node_size);
 
     struct_array[0].value = nums[0];
@@ -39,12 +48,12 @@ int* topKFrequent(int* nums, int num_elements, int k, int* returnSize){
             }
         }
     }
-
+#ifdef DEBUG
     for (int i = 0; i < filled_nodes; i++){
         printf("Value: %d Counter: %d \n", struct_array[i].value, struct_array[i].counter);
     }
     puts("---------------------------------");
-
+#endif
     // if (k >= filled_nodes){
     //     return //
     // }
@@ -64,18 +73,34 @@ int* topKFrequent(int* nums, int num_elements, int k, int* returnSize){
         if (replacements == 0) sorted = 1;
         }
     }
+#ifdef DEBUG
     for (int i = 0; i < filled_nodes; i++){
         printf("Value: %d Counter: %d \n", struct_array[i].value, struct_array[i].counter);
     }
+    puts("---------------------------------");
+#endif
+
+    int *max_values = malloc(k * sizeof(int)); 
+    for(int i = 0; i < k; i++){
+        max_values[i] = struct_array[i].value;
+    }
+    *return_size = k;
+    return max_values;
+
 }
 
 int main(void)
 {
-    int arr[] = {1, 1, 3, 2, 2, 4, 4, 1, 1, 2, 3, 4, 5, 6};
+    int arr[] = {1, 1};
     int num_elements = sizeof(arr) / sizeof(arr[0]);
     int k = 2;
-    int *returnSize;
-    int *out = topKFrequent(arr, num_elements, k, returnSize);
+    int return_size = 0;
+    int *out = topKFrequent(arr, num_elements, k, &return_size);
+
+    printf("Size: %d \n", return_size);
+    for(int i = 0; i < return_size; i++){
+        printf("%d ", out[i]);
+    }
 
     return EXIT_SUCCESS;
 }
