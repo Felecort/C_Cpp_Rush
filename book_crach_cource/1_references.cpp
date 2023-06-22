7c5a7b2aaf19ba79f2445e8c29a9a9b5d06e5ec2
Felecort <dotpointen@gmail.com>
2023-06-22 22:49:53 +0300

    ref for struct

--- book_crach_cource/1_references.cpp
+++ book_crach_cource/1_references.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using std::cout;
 using std::cin;
@@ -10,19 +11,16 @@ struct HolmesIV
     int sence_of_humor;
 };
 
-HolmesIV& not_dincum(){
-    HolmesIV *mike = new HolmesIV();
-    // mike->is_sentient
+HolmesIV& not_dincum() {
+    HolmesIV *mike = new HolmesIV;
     mike->is_sentient = true;
     mike->sence_of_humor = 20;
-    return (HolmesIV&)mike;
+    return *mike;
 }
 
-int main(void)
-{
-    
-    HolmesIV &mike_ref = not_dincum();
-    cout << mike_ref.is_sentient << " " << mike_ref.sence_of_humor << endl;
-    
+int main() {
+    HolmesIV& holmes = not_dincum();
+    cout << holmes.is_sentient << " " << holmes.sence_of_humor << endl;
+    delete &holmes; // Remember to delete the dynamically allocated object
     return 0;
 }
