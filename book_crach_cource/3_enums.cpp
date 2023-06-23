ac922264a5b39d2d0e4d252e196a77bbeda54874
Felecort <dotpointen@gmail.com>
2023-06-24 00:13:50 +0300

    enums

---
+++ book_crach_cource/3_enums.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+enum class Motion {
+    Forward,
+    Back,
+    Left,
+    Right,
+};
+
+int main(void)
+{
+    Motion m = Motion::Back;
+    
+    
+    
+    return 0;
+}
